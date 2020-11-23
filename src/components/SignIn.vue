65018bbfbf7c5608e27d3771843fed58cb83ba29
EC2 Default User <ec2-user@ip-172-31-6-13.ap-northeast-1.compute.internal>
2020-11-23 02:56:04 +0000

    built application

---
+++ src/components/SignIn.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class>
+    <amplify-authenticator username-alias="email">
+      <!-- The rest of your app code -->
+      <amplify-sign-up slot="sign-up" username-alias="email" :form-fields.prop="formFields"></amplify-sign-up>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formFields: [
+        {
+          type: "email",
+          label: "Email",
+          placeholder: "",
+          required: true
+        },
+        {
+          type: "password",
+          label: "Password",
+          placeholder: "",
+          required: true
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,83 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+import SignIn from "../components/SignIn.vue";
+
+import { Hub } from "@aws-amplify/core";
+import Auth from "@aws-amplify/auth";
+
+import UserDataStore from "@/store/index";
+
+Vue.use(VueRouter);
+
+let user;
+
+function getUser() {
+  return Auth.currentAuthenticatedUser()
+    .then((data) => {
+      if (data && data.signInUserSession) {
+        UserDataStore.commit("setUser", data);
+        return data;
+      }
+    })
+    .catch((e) => {
+      console.error(e);
+      UserDataStore.commit("setUser", null);
+      return null;
+    });
+}
+
+Hub.listen("auth", async (data) => {
+  switch (data.payload.event) {
+    case "signIn":
+      user = await getUser();
+      router.push({ path: "/" });
+      break;
+    case "signOut":
+      user = null;
+      UserDataStore.commit("setUser", null);
+      router.push({ path: "/signin" });         // 1
+      break;
+  }
+});
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: { requiresAuth: true },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
-]
+    path: "/signin",                          // 1
+    name: "SignIn",
+    component: SignIn,
+    meta: { requiresAuth: false },
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeResolve(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    user = await getUser();
+    // console.log(user);
+    if (!user) {
+      return next({
+        path: "/signin",                   // 1
+        query: {
+          redirect: to.fullPath,
+        },
+      });
+    }
+    return next();
+  }
+  return next();
+});
+
+export default router;
